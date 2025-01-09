b792f1f2f5dbda062d84dc232d1783ea113a5d43
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-09 16:08:45 +0100

    Milestone 2

--- controller/postsController.js
+++ controller/postsController.js
@@ -3,15 +3,13 @@ const connection = require("../data/conn");
 
 // * index
 function index(req, res) {
-  const sql = "SELECT * FROM `posts`";
+  const sql = "SELECT * FROM `posts`.`posts`";
 
   // eseguo la query
   connection.query(sql, (err, results) => {
-    // if (err) return res.status(500).json({ error: "Database query failed" });
+    if (err) return res.status(500).json({ error: "Database query failed" });
     res.json(results);
-    console.log(results);
   });
-  console.log("index");
 }
 
 // * show
